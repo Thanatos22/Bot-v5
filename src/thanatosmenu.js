905e06a3a965a7c4ed1b20be339d536f92140012
Thanatos22 <76633120+Thanatos22@users.noreply.github.com>
2021-03-04 19:43:12 -0300

    Add files via upload

---
+++ src/thanatosmenu.js
@@ -0,0 +1,32 @@
+const thanatosmenu = (prefix) => {
+	return ` 
+╔═══════════════════
+║  【⛤ꦿ𝙏𝙝𝙖𝙣𝙖𝙩𝙤𝙨 𝙈𝙚𝙣𝙪⛤ꦿ𖣘
+╠═══════════════════
+║╭──❉ * ⛤𝘾𝙤𝙢𝙖𝙣𝙙𝙤𝙨 𝙙𝙤 𝙥𝙖𝙥𝙖𝙞 🥰⛤ * ❉──
+
+║│↭ _*prefix*  [ * ${prefix} * ] 
+
+║│↭_*   [ *${prefix}setprefix* ] 
+║│↭_*   [ *${prefix}clearall* ] 
+║│↭_*   [ *${prefix}clone* ] 
+║│↭_*   [ *${prefix}sorteio* ]  (𝙋𝙑)
+║│↭_*   [ *${prefix}setreply* ] 
+║│↭_*   [ *${prefix}block* ] 
+║│↭_*   [ *${prefix}unblock* ] 
+║│↭_*   [ *${prefix}giftlimit* ] 
+║│↭_*   [ *${prefix}mineração 1/0* ] 
+║│↭_*   [ *${prefix}antilink on/off* ] 
+║│↭_*   [ *${prefix}bc* ] 
+║│↭_*   [ *${prefix}bcgp* ] 
+║│↭_*   [ *${prefix}kickall* ] (𝘽𝘼𝙉)
+║│↭_*   [ *${prefix}leave* ] 
+║│↭_*   [ *${prefix}setpp* ] 
+║│↭_*   [ *${prefix}setmemlimit* ] 
+║│↭_*   [ *${prefix}start* ] 
+║│↭_*   [ *${prefix}%* ]
+║
+╰───────────`
+}
+
+exports.thanatosmenu = thanatosmenu
